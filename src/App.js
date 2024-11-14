6bf0bf5a445bb2520318d606a25b8aeb40ad5595
vinicius-p-sousa <vinicius.pereira.d.sousa@gmail.com>
2024-11-13 22:14:06 -0300

    adding routes.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
 import { routes } from './routes';
-import Home from './pages/Home/Home';
+
+const router = createBrowserRouter(routes);
+
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-
-      <Routes>
-        <Route path="/register" element={<Register />} />
-      </Routes>
-
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 };
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Product from "./pages/Product/Product";
 
 export const routes = [
   { path: "/", element: <Login /> },
   { path: "/home", element: <Home /> },
   { path: "/Register", element: <Register /> },
+  { path: "/product", element: <Product /> },
 ];
